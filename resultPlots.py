d4bb16ea79b7d9c2b6e8cc6ed2b8819ffce8cda8
binocularity <nick@DevBox2.xurrh2b5ik1u5ku44sveynitie.bx.internal.cloudapp.net>
2018-12-01 12:05:05 +0000

    Plotting started supported

--- resultPlots.py
+++ resultPlots.py
@@ -1,3 +1,26 @@
 import pandas as pd
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 import os
 
+dataFolder = os.getcwd() + '/DataWrangling/20181108Terascope_01/'
+
+inFile=dataFolder+'summaryStats_1024_20181108.csv'
+outFile = dataFolder+"neasuredSPlot.svg"
+
+plotDf = pd.read_csv( inFile, index_col = 0 )
+
+fg = plt.figure()
+thePlot = fg.gca()
+thePlot.plot( plotDf['runId'], plotDf['measuredS'], color='lime', marker = '.', markeredgecolor = 'black', markerfacecolor = 'black' )
+plt.xlabel('runId')
+plt.ylabel('measuredS')
+plt.xlim((0,1200))
+
+for i,j in plotDf[['runId','measuredS']].values:
+    if ( (i < 65) or (i > 112) ):
+        plt.annotate( '{:5.1f}'.format(j), xy=(i+20,j) )
+
+plt.draw() # necessary to render figure before saving
+fg.savefig(outFile, bbox_inches='tight')
